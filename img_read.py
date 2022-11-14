d0387d086c9fe44eab6d8a53c3eac433a23f65cd
dlwnstn0524 <ljs052450@gmail.com>
2022-11-15 02:00:07 +0900

    1st_study_init

---
+++ img_read.py
@@ -0,0 +1,11 @@
+import cv2
+
+img_file = "./img/christmas.jpg"
+img = cv2.imread(img_file, cv2.IMREAD_GRAYSCALE)    # 회색으로 읽기
+
+if img is not None:
+  cv2.imshow('IMG', img)
+  cv2.waitKey()
+  cv2.destroyAllWindows()
+else:
+    print('No image file.')
